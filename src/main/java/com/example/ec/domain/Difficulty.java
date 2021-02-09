00942a7a2f53b73fcb5bd6b948e63cff5b7b5385
Pedro Henrique Carvalho <pedrohj.carvalho@gmail.com>
2021-02-09 15:26:33 -0500

    First Commit

---
+++ src/main/java/com/example/ec/domain/Difficulty.java
@@ -0,0 +1,14 @@
+package com.example.ec.domain;
+
+/**
+ * Created by Pedro Henrique Carvalho
+ */
+
+/**
+ * Enumeration of the level of effort.
+ *
+ * Created by Pedro Henrique Carvalho
+ */
+public enum Difficulty {
+    Easy, Medium, Difficult, Varies;
+}
