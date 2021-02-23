5e936fd3e946f7255d475d38f56ee2583224999b
Pedro Henrique Carvalho <pedrohj.carvalho@gmail.com>
2021-02-23 13:43:19 -0500

    Adding Pageable

--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -1,7 +1,12 @@
 package com.example.ec.repo;
 
 import com.example.ec.domain.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
 
 
 /**
@@ -9,5 +14,6 @@ import org.springframework.data.repository.CrudRepository;
  *
  * Created by Pedro Henrique Carvalho
  */
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+    Page<Tour> findByTourPackageCode(String code, Pageable pageable);
 }
